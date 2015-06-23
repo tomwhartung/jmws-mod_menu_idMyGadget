6488a78ab25e8903f406a4544b18152392b77a8e
Tom W Hartung <tomwhartung@gmail.com>
2015-06-23 14:01:00 -0600

    Updated default.php to not display a menu on phones.

--- modules/mod_menu_idmygadget/tmpl/default.php
+++ modules/mod_menu_idmygadget/tmpl/default.php
@@ -8,98 +8,104 @@
  */
 
 defined('_JEXEC') or die;
+global $jmwsIdMyGadget;
 
 // Note. It is important to remove spaces between elements.
 ?>
 <?php // The menu class is deprecated. Use nav instead. ?>
-<ul class="nav menu<?php echo $class_sfx;?>"<?php
-	$tag = '';
 
-	if ($params->get('tag_id') != null)
-	{
-		$tag = $params->get('tag_id') . '';
-		echo ' id="' . $tag . '"';
-	}
-?>>
-<?php
-foreach ($list as $i => &$item)
-{
-	$class = 'item-' . $item->id;
+<?php if ( $jmwsIdMyGadget->getGadgetString() === JmwsIdMyGadget::GADGET_STRING_DESKTOP ) : ?>
+	<ul class="nav menu<?php echo $class_sfx;?>"<?php
+		$tag = '';
 
-	if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
+		if ($params->get('tag_id') != null)
+		{
+			$tag = $params->get('tag_id') . '';
+			echo ' id="' . $tag . '"';
+		}
+	?>>
+	<?php
+	foreach ($list as $i => &$item)
 	{
-		$class .= ' current';
-	}
+		$class = 'item-' . $item->id;
 
-	if (in_array($item->id, $path))
-	{
-		$class .= ' active';
-	}
-	elseif ($item->type == 'alias')
-	{
-		$aliasToId = $item->params->get('aliasoptions');
+		if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
+		{
+			$class .= ' current';
+		}
 
-		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+		if (in_array($item->id, $path))
 		{
 			$class .= ' active';
 		}
-		elseif (in_array($aliasToId, $path))
+		elseif ($item->type == 'alias')
 		{
-			$class .= ' alias-parent-active';
+			$aliasToId = $item->params->get('aliasoptions');
+
+			if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+			{
+				$class .= ' active';
+			}
+			elseif (in_array($aliasToId, $path))
+			{
+				$class .= ' alias-parent-active';
+			}
+		}
+
+		if ($item->type == 'separator')
+		{
+			$class .= ' divider';
+		}
+
+		if ($item->deeper)
+		{
+			$class .= ' deeper';
+		}
+
+		if ($item->parent)
+		{
+			$class .= ' parent';
+		}
+
+		if (!empty($class))
+		{
+			$class = ' class="' . trim($class) . '"';
+		}
+
+		echo '<li' . $class . '>';
+
+		// Render the menu item.
+		switch ($item->type) :
+			case 'separator':
+			case 'url':
+			case 'component':
+			case 'heading':
+				require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+				break;
+
+			default:
+				require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+				break;
+		endswitch;
+
+		// The next item is deeper.
+		if ($item->deeper)
+		{
+			echo '<ul class="nav-child unstyled small">';
+		}
+		elseif ($item->shallower)
+		{
+			// The next item is shallower.
+			echo '</li>';
+			echo str_repeat('</ul></li>', $item->level_diff);
+		}
+		else
+		{
+			// The next item is on the same level.
+			echo '</li>';
 		}
 	}
-
-	if ($item->type == 'separator')
-	{
-		$class .= ' divider';
-	}
-
-	if ($item->deeper)
-	{
-		$class .= ' deeper';
-	}
-
-	if ($item->parent)
-	{
-		$class .= ' parent';
-	}
-
-	if (!empty($class))
-	{
-		$class = ' class="' . trim($class) . '"';
-	}
-
-	echo '<li' . $class . '>';
-
-	// Render the menu item.
-	switch ($item->type) :
-		case 'separator':
-		case 'url':
-		case 'component':
-		case 'heading':
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-			break;
-
-		default:
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-			break;
-	endswitch;
-
-	// The next item is deeper.
-	if ($item->deeper)
-	{
-		echo '<ul class="nav-child unstyled small">';
-	}
-	elseif ($item->shallower)
-	{
-		// The next item is shallower.
-		echo '</li>';
-		echo str_repeat('</ul></li>', $item->level_diff);
-	}
-	else
-	{
-		// The next item is on the same level.
-		echo '</li>';
-	}
-}
-?></ul>
+	?></ul>
+<?php else : ?>
+	<p>No menu on phones and tablets</p>
+<?php endif; ?>
