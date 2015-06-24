bb59b417a826effb760dd4fdfd91e084a29fad8b
Tom W Hartung <tomwhartung@gmail.com>
2015-06-24 13:56:49 -0600

    Fixed a comment.

--- modules/mod_menu_idmygadget/tmpl/phoneHeaderNav.php
+++ modules/mod_menu_idmygadget/tmpl/phoneHeaderNav.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  mod_menu
+ * @subpackage  mod_menu_idmygadget
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
