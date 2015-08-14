ac165d81f6c84b772840939018712bba50076c90
Tom W Hartung <tomwhartung@gmail.com>
2015-08-14 15:42:38 -0600

    Reverted default.php back to the version in mod_menu, because adding device detection did not really achieve the goal I was hoping and I decided to split off position-7 into position-7-phone and position-7-tablet anyway for better control and that seems to work better so yeah.

--- modules/mod_menu_idmygadget/tmpl/default.php
+++ modules/mod_menu_idmygadget/tmpl/default.php
@@ -8,105 +8,98 @@
  */
 
 defined('_JEXEC') or die;
-global $jmwsIdMyGadget;
 
 // Note. It is important to remove spaces between elements.
 ?>
 <?php // The menu class is deprecated. Use nav instead. ?>
+<ul class="nav menu<?php echo $class_sfx;?>"<?php
+	$tag = '';
 
-<?php
-	if ( $jmwsIdMyGadget->getGadgetString() === $jmwsIdMyGadget::GADGET_STRING_DESKTOP ||
-	     $jmwsIdMyGadget->getGadgetString() === $jmwsIdMyGadget::GADGET_STRING_TABLET ) :
-?>
-	<ul class="nav menu<?php echo $class_sfx;?>"<?php
-		$tag = '';
-
-		if ($params->get('tag_id') != null)
-		{
-			$tag = $params->get('tag_id') . '';
-			echo ' id="' . $tag . '"';
-		}
-	?>>
-	<?php
-	foreach ($list as $i => &$item)
+	if ($params->get('tag_id') != null)
 	{
-		$class = 'item-' . $item->id;
+		$tag = $params->get('tag_id') . '';
+		echo ' id="' . $tag . '"';
+	}
+?>>
+<?php
+foreach ($list as $i => &$item)
+{
+	$class = 'item-' . $item->id;
 
-		if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
-		{
-			$class .= ' current';
-		}
+	if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
+	{
+		$class .= ' current';
+	}
 
-		if (in_array($item->id, $path))
+	if (in_array($item->id, $path))
+	{
+		$class .= ' active';
+	}
+	elseif ($item->type == 'alias')
+	{
+		$aliasToId = $item->params->get('aliasoptions');
+
+		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
 		{
 			$class .= ' active';
 		}
-		elseif ($item->type == 'alias')
+		elseif (in_array($aliasToId, $path))
 		{
-			$aliasToId = $item->params->get('aliasoptions');
-
-			if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
-			{
-				$class .= ' active';
-			}
-			elseif (in_array($aliasToId, $path))
-			{
-				$class .= ' alias-parent-active';
-			}
-		}
-
-		if ($item->type == 'separator')
-		{
-			$class .= ' divider';
-		}
-
-		if ($item->deeper)
-		{
-			$class .= ' deeper';
-		}
-
-		if ($item->parent)
-		{
-			$class .= ' parent';
-		}
-
-		if (!empty($class))
-		{
-			$class = ' class="' . trim($class) . '"';
-		}
-
-		echo '<li' . $class . '>';
-
-		// Render the menu item.
-		switch ($item->type) :
-			case 'separator':
-			case 'url':
-			case 'component':
-			case 'heading':
-				require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-				break;
-
-			default:
-				require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-				break;
-		endswitch;
-
-		// The next item is deeper.
-		if ($item->deeper)
-		{
-			echo '<ul class="nav-child unstyled small">';
-		}
-		elseif ($item->shallower)
-		{
-			// The next item is shallower.
-			echo '</li>';
-			echo str_repeat('</ul></li>', $item->level_diff);
-		}
-		else
-		{
-			// The next item is on the same level.
-			echo '</li>';
+			$class .= ' alias-parent-active';
 		}
 	}
-	?></ul>
-<?php endif; ?>
+
+	if ($item->type == 'separator')
+	{
+		$class .= ' divider';
+	}
+
+	if ($item->deeper)
+	{
+		$class .= ' deeper';
+	}
+
+	if ($item->parent)
+	{
+		$class .= ' parent';
+	}
+
+	if (!empty($class))
+	{
+		$class = ' class="' . trim($class) . '"';
+	}
+
+	echo '<li' . $class . '>';
+
+	// Render the menu item.
+	switch ($item->type) :
+		case 'separator':
+		case 'url':
+		case 'component':
+		case 'heading':
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+			break;
+
+		default:
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+			break;
+	endswitch;
+
+	// The next item is deeper.
+	if ($item->deeper)
+	{
+		echo '<ul class="nav-child unstyled small">';
+	}
+	elseif ($item->shallower)
+	{
+		// The next item is shallower.
+		echo '</li>';
+		echo str_repeat('</ul></li>', $item->level_diff);
+	}
+	else
+	{
+		// The next item is on the same level.
+		echo '</li>';
+	}
+}
+?></ul>
